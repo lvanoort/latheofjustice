2ac14b2a91c76b393bbf46aa5b1dae868a8b21ba
derek <derek@robot.(none)>
2014-03-25 15:56:43 -0400

    Added homing stuff.

--- Hello_Galil/hello.cpp
+++ Hello_Galil/hello.cpp
@@ -13,7 +13,7 @@ Galil * galil_connection = NULL;
 bool setup() {
   //TODO: make setable variable
   galil_connection = new Galil("/dev/ttyUSB0 115200");
-  galil_connection->command("ST; SH; MT2,2,2; ST; AC 60000,,60000; DC 60000,,60000; JG0,0,0; BG ABC;");
+  galil_connection->command("ST; SH;MT2,2,2; ST; AC 60000,,60000; DC 60000,,60000; JG0,0,0; BG ABC;");
   return true;
 }
 
@@ -21,6 +21,17 @@ bool homeSwitchOff() {
   if(!galil_connection)
     return 0;
   return galil_connection->commandValue("MG_HMC");
+}
+
+bool returnToHome() {
+if(!galil_connection)
+    return false;
+    galil_connection->command("ST; LD 3,3,3; CN,1; SPC=22000; HM C; BG C;");
+	sleep(5);
+
+  //TODO: Reset encoders
+  galil_connection->command("SH; LD0,0,0; JG0,0,0; BG C;");
+  return true;
 }
 
 double read_axis1()
@@ -34,7 +45,7 @@ void write_axis1(double val)
 {
   if(!galil_connection)
     return;
-    
+   
   int num = floor(val);
   ostringstream convert;
   convert<<num;
@@ -56,21 +67,24 @@ int main()
       usleep(10000);
     }*/
 
-    /*galil_connection->command("ST; CN,1; SPC=1000; HM C; BG C;");
+ //   galil_connection->command("ST; CN,1; SPC=1000; HM C; BG C;");
+    returnToHome();
 
-    sleep(10);
+    //sleep(10);
 
-    galil_connection->command("ST; JG0,0,0; BG C;");*/
+    /*galil_connection->command("ST; JG0,0,0; BG C;");*/
 
-    /*for(int i = 0; i < 250; i++) {
+    for(int i = 0; i < 500; i++) {
       cout << "Got: " << read_axis1() <<std::endl;
-      write_axis1(-3000);
+	cout << homeSwitchOff() <<std::endl;      
+	write_axis1(-3000);
       usleep(10000);
-    }*/
+    }
 
-    for(int i = 0; i < 750; i++) {
+    for(int i = 0; i < 250; i++) {
       cout << "Got: " << read_axis1() <<std::endl;
-      write_axis1(3000);
+cout << homeSwitchOff() <<std::endl;      
+write_axis1(3000);
       usleep(10000);
     }
 
